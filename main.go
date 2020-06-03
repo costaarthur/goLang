f81d525c0441c6b94d584752e965e27a88c98c48
costaarthur <costaarthur@hotmail.com>
2020-06-03 14:03:03 -0300

    user data added header content type changed

--- main.go
+++ main.go
@@ -9,37 +9,61 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-type Articles []Article
-
-func getArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Title", Desc: "Test Desc", Content: "Hail clã"},
-	}
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+type User struct {
+	// add `json` to set prop name when convert to json
+	Id       int     `json:"id"`
+	Name     string  `json:"name"`
+	LastName string  `json:"lastName"`
+	Age      int     `json:"age"`
+	Height   float32 `json:"height"`
 }
 
-func postArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test POST endpoint worked")
+var Users []User = []User{
+	User{
+		Id:       1,
+		Name:     "John",
+		LastName: "Locke",
+		Age:      45,
+		Height:   1.75,
+	},
+	User{
+		Id:       2,
+		Name:     "Jack",
+		LastName: "Shephard",
+		Age:      40,
+		Height:   1.72,
+	},
+	User{
+		Id:       3,
+		Name:     "James",
+		LastName: "Ford",
+		Age:      39,
+		Height:   1.77,
+	},
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(Users)
+}
+
+func postUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test POST endpoint worked")
+}
+
 func handlerRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", getArticles).Methods("GET")
-	myRouter.HandleFunc("/articles", postArticles).Methods("POST")
+	myRouter.HandleFunc("/users", getUsers).Methods("GET")
+	myRouter.HandleFunc("/users", postUsers).Methods("POST")
 	log.Fatal(http.ListenAndServe(":3003", myRouter))
 }
 
 func main() {
+	fmt.Println("Server running on the port 3003")
 	handlerRequests()
 }
